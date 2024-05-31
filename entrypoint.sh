f818ff1d1b6ae1195ee7933d220631d8aebd0b0d
Deepak Chourasia <dchouras@redhat.com>
2024-05-31 13:42:44 +0530

    fixing duplicate branch name issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,11 +32,11 @@ echo "UPSTREAM_REPO=$UPSTREAM_REPO"
 
 if [[ $DOWNSTREAM_REPO == "GITHUB_REPOSITORY" ]]
 then
-  git clone "https://github.com/${GITHUB_REPOSITORY}.git" work
+  git clone "https://github.com/${GITHUB_REPOSITORY}.git" --branch ${DOWNSTREAM_BRANCH} work
   cd work || { echo "Missing work dir" && exit 2 ; }
   git remote set-url origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 else
-  git clone $DOWNSTREAM_REPO work
+  git clone $DOWNSTREAM_REPO --branch ${DOWNSTREAM_BRANCH} work
   cd work || { echo "Missing work dir" && exit 2 ; }
   git remote set-url origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${DOWNSTREAM_REPO/https:\/\/github.com\//}"
 fi
